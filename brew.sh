9c20e604b44243ba7d34931bcd874d93ebbd764a
k <k@field.fx>
2019-09-25 11:00:56 -0400

    Disable various homebrew packages from initial bootstrapping

--- brew.sh
+++ brew.sh
@@ -13,40 +13,35 @@ brew install grep
 brew install ctags
 brew install cscope
 brew install the_silver_searcher
-brew install openssh
 brew install wget
-brew install lftp
-
-brew install bash
-brew install bash-completion2
 
 brew install vim --with-override-system-vi
 brew install tmux
 
-brew install aircrack-ng
-brew install bfg
+#brew install aircrack-ng
+#brew install bfg
 brew install binutils
 brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
+#brew install cifer
+#brew install dex2jar
+#brew install dns2tcp
+#brew install fcrackzip
+#brew install foremost
+#brew install hashpump
+#brew install hydra
+#brew install john
+#brew install knock
+#brew install netpbm
 brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
+#brew install pngcheck
+#brew install socat
+#brew install sqlmap
 brew install tcpflow
 brew install tcpreplay
 brew install tcptrace
-brew install ucspi-tcp
-brew install xpdf
-brew install xz
+#brew install ucspi-tcp
+#brew install xpdf
+#brew install xz
 
 brew install ack
 brew install git
@@ -57,13 +52,13 @@ brew install vbindiff
 brew install p7zip
 brew install openvpn
 
-brew install go
-brew install java
+#brew install go
+#brew install java
 
 brew cask install google-chrome
 brew cask install iterm2
 brew cask install wireshark
-brew cask install osxfuse
+#brew cask install osxfuse
     
 brew install --HEAD usbmuxd
 brew install --HEAD libimobiledevice
