dc8e938575b30ec34da9f83316fae4d3e5649c04
kryptos <j.s.alexander@gmail.com>
2019-10-27 14:50:29 -0700

    Add ubuntu bootstrapping script; bootstrap dotfiles to home dir; rename brew.sh to macos.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+cp -rf .vim ~/
+cp -rf .vimrc ~/
+cp -rf .tmux.conf ~/
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
---
+++ ubuntu.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo apt install vim
+sudo apt install tmux
+sudo apt install ctags
+sudo apt install cscope
+sudo apt install git
