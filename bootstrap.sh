8e383ab8fae308b3f3011ba2e046f9af3efb2cdc
kryptos <j.s.alexander@gmail.com>
2020-01-12 19:33:52 -0500

    Remove .vim dir; bootstrap plugins in vim immediately

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-cp -rf .vim ~/
+#cp -rf .vim ~/
 cp -rf .vimrc ~/
 cp -rf .tmux.conf ~/
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+vim -c "PluginInstall" +qall
